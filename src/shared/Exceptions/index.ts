bb9597910fb3ae6273ab60b855e86d30d085e6cc
elbotanist <ahmedelgaidi260@gmail.com>
2023-05-10 03:35:46 +0300

    Worked on ceneralizing exceptions

---
+++ src/shared/Exceptions/index.ts
@@ -0,0 +1,15 @@
+import { BadRequestException } from "./Common/badRequestException";
+import { ForbiddenException } from "./Common/forbiddenException";
+import { InternalServerException } from "./Common/InternalServerException";
+import { NotFoundException } from "./Common/notFoundException";
+import { UnAuthorizedException } from "./Common/unAuthorizedException";
+import { ValidationException } from "./Common/validationException";
+
+export {
+	BadRequestException,
+	ForbiddenException,
+	InternalServerException,
+	NotFoundException,
+	UnAuthorizedException,
+	ValidationException,
+};
