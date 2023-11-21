b6efe5f6b4e5c94397a23c81d764692439db2563
elbotanist <ahmedelgaidi260@gmail.com>
2023-11-21 08:57:49 +0200

    My first commit for my nestjs setup to master branch

---
+++ server-with-nestjs/dist/app.module.js
@@ -0,0 +1,42 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const mongoose_1 = require("@nestjs/mongoose");
+const post_module_1 = require("./modules/post/post.module");
+const user_module_1 = require("./modules/user/user.module");
+const config_1 = require("@nestjs/config");
+let AppModule = class AppModule {
+    constructor() {
+        console.log({ env: process.env.MONGO_URI });
+    }
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+                envFilePath: `.env.${process.env.NODE_ENV}`,
+            }),
+            mongoose_1.MongooseModule.forRoot('mongodb+srv://ahmedelgaidi:81zO2YXL5zQtUDr0@cluster0.b1lyle4.mongodb.net/blog'),
+            post_module_1.PostModule,
+            user_module_1.UserModule,
+        ],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    }),
+    __metadata("design:paramtypes", [])
+], AppModule);
+//# sourceMappingURL=app.module.js.map
---
+++ server-with-nestjs/dist/modules/post/controllers/post.controller.d.ts
@@ -0,0 +1,3 @@
+export declare class PostController {
+    sayHi(): Promise<string>;
+}
