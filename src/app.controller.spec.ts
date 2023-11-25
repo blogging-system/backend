527eac7e2c92f811283d80b33a3af110fab9e43e
elbotanist <ahmedelgaidi260@gmail.com>
2023-11-25 14:54:28 +0200

    Preparing for onRender deployment

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello World!!"', () => {
       expect(appController.getHello()).toBe('Hello World!')
     })
   })
