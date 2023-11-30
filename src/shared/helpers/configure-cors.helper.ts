7ddd37b70fa34801cdb150d1b89e6b7a5eca104e
elbotanist <ahmedelgaidi260@gmail.com>
2023-11-30 12:13:32 +0200

    Try 5 for CORS error

--- src/shared/helpers/configure-cors.helper.ts
+++ src/shared/helpers/configure-cors.helper.ts
@@ -1,20 +1,13 @@
-import { INestApplication, UnauthorizedException } from '@nestjs/common'
+import { INestApplication } from '@nestjs/common'
 import { EnvironmentType } from '../enums'
 import { appConfig } from '../config'
 
 export const configureCors = (app: INestApplication): void => {
   appConfig.environment.env === EnvironmentType.PRODUCTION
     ? app.enableCors({
-        // origin: (origin, cb) => {
-        //   if (appConfig.allowedOrigins.includes(origin)) {
-        //     cb(null, origin)
-        //   } else {
-        //     throw new UnauthorizedException('Not Allowed origins!')
-        //   }
-        // },
-        origin: 'https://admin.ahmedelgaidi.com',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        preflightContinue: true,
+        origin: appConfig.allowedOrigins,
+        methods: ['GET', 'PATCH', 'POST', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
       })
     : app.enableCors()
