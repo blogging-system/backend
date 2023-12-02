17fd26ad426be36867d28b7bd32caa0a12b9b14f
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-02 02:10:23 +0200

    Wrote tests for "public-tag.controller"

---
+++ src/modules/tag/controllers/public/public-tag.controller.spec.ts
@@ -0,0 +1,48 @@
+import { PublicTagController } from './public-tag.controller'
+import { Test, TestingModule } from '@nestjs/testing'
+import { TagService } from '../../services'
+import { Tag } from '../../schemas'
+
+describe('🌐 PublicTagController | Controller Layer', () => {
+  let publicTagController: PublicTagController
+  let tagService: TagService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PublicTagController],
+      providers: [
+        {
+          provide: TagService,
+          useValue: {
+            getAllTags: jest.fn(),
+          },
+        },
+      ],
+    }).compile()
+
+    publicTagController = module.get<PublicTagController>(PublicTagController)
+    tagService = module.get<TagService>(TagService)
+  })
+
+  describe('getAllTags method', () => {
+    it('should return all tags', async () => {
+      const expectedResult: Tag[] = [
+        {
+          _id: 'tag1',
+          name: 'Tag 1',
+        },
+        {
+          _id: 'tag2',
+          name: 'Tag 2',
+        },
+      ]
+
+      ;(tagService.getAllTags as jest.Mock).mockResolvedValueOnce(expectedResult)
+
+      const result = await publicTagController.getAllTags()
+
+      expect(tagService.getAllTags).toHaveBeenCalled()
+      expect(result).toEqual(expectedResult)
+    })
+  })
+})
