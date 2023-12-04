dbbd63f10eef88522471703d64b3f26e70e70a08
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-04 22:25:39 +0200

    wrote e2e tests for the logout endpoint

--- test/auth/private/whoami.auth.e2e-spec.ts
+++ test/auth/private/whoami.auth.e2e-spec.ts
@@ -6,9 +6,12 @@ import * as request from 'supertest'
 
 describe('🏠 Auth Module (E2E Tests)', () => {
   let app: INestApplication
-  const loginPath: string = '/auth/login'
-  const path: string = '/admin/auth/whoami'
-  const method: string = 'GET'
+  let loginPath: string = '/auth/login',
+    whoAmIPath: string = '/admin/auth/whoami',
+    logOutPath: string = '/admin/auth/logout'
+
+  let whoAmIMethod: string = 'GET',
+    logOutMethod: string = 'POST'
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -20,14 +23,14 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     await app.init()
   })
 
-  describe(`➡ "${path}" (${method})`, () => {
+  describe(`➡ "${whoAmIPath}" (${whoAmIMethod})`, () => {
     it(`Should be a private endpoint`, async () => {
-      const { status, body } = await request(app.getHttpServer()).get(path)
+      const { status } = await request(app.getHttpServer()).get(whoAmIPath)
 
       expect(status).toBe(401)
     })
 
-    it(`Should return 201 and current user data`, async () => {
+    it(`Should return 200 and current user data`, async () => {
       const loginPayload = { email: appConfig.seeders.rootUser.email, password: appConfig.seeders.rootUser.password }
 
       const {
@@ -35,11 +38,34 @@ describe('🏠 Auth Module (E2E Tests)', () => {
       } = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       const { status, body } = await request(app.getHttpServer())
-        .get(path)
+        .get(whoAmIPath)
         .set('Authorization', `Bearer ${accessToken}`)
 
       expect(status).toBe(200)
       expect(Object.keys(body).length).toBe(4)
+    })
+  })
+
+  describe(`➡ "${logOutPath}" (${logOutMethod})`, () => {
+    it(`Should be a private endpoint`, async () => {
+      const { status } = await request(app.getHttpServer()).post(logOutPath)
+
+      expect(status).toBe(401)
+    })
+
+    it(`Should return 200 and success message`, async () => {
+      const loginPayload = { email: appConfig.seeders.rootUser.email, password: appConfig.seeders.rootUser.password }
+
+      const {
+        body: { accessToken },
+      } = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
+
+      const { status, body } = await request(app.getHttpServer())
+        .post(logOutPath)
+        .set('Authorization', `Bearer ${accessToken}`)
+
+      expect(status).toBe(200)
+      expect(body.message).toBeDefined()
     })
   })
 })
