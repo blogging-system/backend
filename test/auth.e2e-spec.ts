db23c7ead928a8e1070a593d57d1adfc210e93af
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-04 23:06:38 +0200

    Added some updates to the tests architechture

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -6,8 +6,13 @@ import * as request from 'supertest'
 
 describe('🏠 Auth Module (E2E Tests)', () => {
   let app: INestApplication
-  const path: string = '/auth/login'
-  const method: string = 'POST'
+  let loginPath: string = '/auth/login',
+    whoAmIPath: string = '/admin/auth/whoami',
+    logOutPath: string = '/admin/auth/logout'
+
+  let loginMethod: string = 'POST',
+    whoAmIMethod: string = 'GET',
+    logOutMethod: string = 'POST'
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,11 +24,11 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     await app.init()
   })
 
-  describe(`➡ "${path}" (${method})`, () => {
+  describe(`➡ "${loginPath}" (${loginMethod})`, () => {
     it(`Should return 201 and created session tokens`, async () => {
       const loginPayload = { email: appConfig.seeders.rootUser.email, password: appConfig.seeders.rootUser.password }
 
-      const { status, body } = await request(app.getHttpServer()).post(path).send(loginPayload)
+      const { status, body } = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(status).toBe(201)
       expect(body.accessToken).toBeDefined()
@@ -34,7 +39,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw UnAuthorizedException for invalid email`, async () => {
       const loginPayload = { email: 'invalidEmail@gmail.com', password: appConfig.seeders.rootUser.password }
 
-      const result = await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(401)
     })
@@ -42,7 +47,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw UnAuthorizedException for invalid password`, async () => {
       const loginPayload = { email: appConfig.seeders.rootUser.email, password: 'invalidPassword' }
 
-      const result = await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
       expect(result.status).toBe(401)
     })
 
@@ -50,16 +55,16 @@ describe('🏠 Auth Module (E2E Tests)', () => {
       const loginPayload = { email: appConfig.seeders.rootUser.email, password: 'invalidPassword' }
 
       for (let i = 0; i < 5; i++) {
-        await request(app.getHttpServer()).post(path).send(loginPayload)
+        await request(app.getHttpServer()).post(loginPath).send(loginPayload)
       }
 
-      const { status, body } = await request(app.getHttpServer()).post(path).send(loginPayload)
+      const { status, body } = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(status).toBe(429)
     })
 
     it(`Should throw BadRequestException if the credentials aren't given`, async () => {
-      const result = await request(app.getHttpServer()).post(path)
+      const result = await request(app.getHttpServer()).post(loginPath)
 
       expect(result.status).toBe(400)
     })
@@ -67,7 +72,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the credentials were empty`, async () => {
       const loginPayload = {}
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
     })
@@ -75,7 +80,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the email is not of type string`, async () => {
       const loginPayload = { email: 1243, password: appConfig.seeders.rootUser.password }
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
     })
@@ -83,7 +88,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the password is not of type string`, async () => {
       const loginPayload = { email: appConfig.seeders.rootUser.password, password: 1234 }
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
     })
@@ -91,7 +96,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the email is too short`, async () => {
       const loginPayload = { email: 'q@g.m', password: appConfig.seeders.rootUser.password }
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
     })
@@ -99,7 +104,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the email is too long`, async () => {
       const loginPayload = { email: `${'1'.repeat(500)}@gmail.com`, password: appConfig.seeders.rootUser.password }
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
     })
@@ -107,7 +112,7 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the password is too short`, async () => {
       const loginPayload = { email: appConfig.seeders.rootUser.password, password: '1' }
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
     })
@@ -115,9 +120,55 @@ describe('🏠 Auth Module (E2E Tests)', () => {
     it(`Should throw BadRequestException if the password is too long`, async () => {
       const loginPayload = { email: appConfig.seeders.rootUser.password, password: '1234'.repeat(500) }
 
-      const result = await await request(app.getHttpServer()).post(path).send(loginPayload)
+      const result = await await request(app.getHttpServer()).post(loginPath).send(loginPayload)
 
       expect(result.status).toBe(400)
+    })
+  })
+
+  describe(`➡ "${whoAmIPath}" (${whoAmIMethod})`, () => {
+    it(`Should be a private endpoint`, async () => {
+      const { status } = await request(app.getHttpServer()).get(whoAmIPath)
+
+      expect(status).toBe(401)
+    })
+
+    it(`Should return 200 and current user data`, async () => {
+      const loginPayload = { email: appConfig.seeders.rootUser.email, password: appConfig.seeders.rootUser.password }
+
+      const {
+        body: { accessToken },
+      } = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
+
+      const { status, body } = await request(app.getHttpServer())
+        .get(whoAmIPath)
+        .set('Authorization', `Bearer ${accessToken}`)
+
+      expect(status).toBe(200)
+      expect(Object.keys(body).length).toBe(4)
+    })
+  })
+
+  describe(`➡ "${logOutPath}" (${logOutMethod})`, () => {
+    it(`Should be a private endpoint`, async () => {
+      const { status } = await request(app.getHttpServer()).post(logOutPath)
+
+      expect(status).toBe(401)
+    })
+
+    it(`Should return 200 and success message`, async () => {
+      const loginPayload = { email: appConfig.seeders.rootUser.email, password: appConfig.seeders.rootUser.password }
+
+      const {
+        body: { accessToken },
+      } = await request(app.getHttpServer()).post(loginPath).send(loginPayload)
+
+      const { status, body } = await request(app.getHttpServer())
+        .post(logOutPath)
+        .set('Authorization', `Bearer ${accessToken}`)
+
+      expect(status).toBe(200)
+      expect(body.message).toBeDefined()
     })
   })
 })
--- test/config/jest.setup.ts
+++ test/config/jest.setup.ts
@@ -1,13 +1,14 @@
 import { appConfig } from '@src/shared/config'
 import * as mongoose from 'mongoose'
 
-beforeAll(async () => {
+beforeEach(async () => {
   await mongoose.connect(appConfig.storage.database.mongodb.uri)
 })
 
-afterAll(async () => {
+afterEach(async () => {
   const collections = await mongoose.connection.db.collections()
 
+  // This is just for not deleting the seeded user!
   for (const collection of collections) {
     if (collection.collectionName !== 'users') {
       await collection.drop()
