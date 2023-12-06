94fde5d1bd2c047d90a9c672dfec67a735431ab9
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-06 14:43:59 +0200

    Worked on refactoring the repository layer of the "Keyword" module

--- src/modules/keyword/repositories/keyword.repository.ts
+++ src/modules/keyword/repositories/keyword.repository.ts
@@ -1,59 +1,12 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common'
-import { DocumentIdType, ResultMessage } from '@src/shared/contracts/types'
+import { BaseRepository } from '@src/shared/repository'
 import { InjectModel } from '@nestjs/mongoose'
-import { CreateKeywordDto } from '../dtos'
-import { MESSAGES } from '../constants'
-import { Model, Types } from 'mongoose'
+import { Injectable } from '@nestjs/common'
 import { Keyword } from '../schemas'
+import { Model } from 'mongoose'
 
 @Injectable()
-export class KeywordRepository {
-  constructor(@InjectModel(Keyword.name) private keywordModel: Model<Keyword>) {}
-
-  public async createOne(data: CreateKeywordDto): Promise<Keyword> {
-    const isKeywordCreated = await this.keywordModel.create(data)
-
-    if (!isKeywordCreated) throw new InternalServerErrorException(MESSAGES.CREATION_FAILED)
-
-    return isKeywordCreated
-  }
-
-  public async updateOne(keywordId: DocumentIdType, payload: CreateKeywordDto): Promise<Keyword> {
-    const isKeywordUpdated = await this.keywordModel.findByIdAndUpdate(keywordId, payload, { new: true })
-
-    if (!isKeywordUpdated) throw new InternalServerErrorException(MESSAGES.UPDATE_FAILED)
-
-    return isKeywordUpdated
-  }
-
-  public async deleteOne(keywordId: DocumentIdType): Promise<ResultMessage> {
-    const isKeywordDeleted = await this.keywordModel.deleteOne({
-      _id: new Types.ObjectId(keywordId),
-    })
-
-    if (isKeywordDeleted.deletedCount === 0) throw new InternalServerErrorException(MESSAGES.DELETE_FAILED)
-
-    return { message: MESSAGES.DELETED_SUCCESSFULLY }
-  }
-
-  public async findOneById(keywordId: DocumentIdType): Promise<Keyword> {
-    const isPostFound = await this.keywordModel.findOne({ _id: keywordId }).lean()
-
-    if (!isPostFound) throw new NotFoundException(MESSAGES.KEYWORD_NOT_FOUND)
-
-    return isPostFound
-  }
-
-  public async findMany(): Promise<Keyword[]> {
-    const areTagsFound = await this.keywordModel.find().lean()
-    if (areTagsFound.length === 0) throw new NotFoundException(MESSAGES.KEYWORDS_NOT_FOUND)
-
-    return areTagsFound
-  }
-
-  public async countDocuments(): Promise<ResultMessage> {
-    const count = await this.keywordModel.countDocuments().lean()
-
-    return { count }
+export class KeywordRepository extends BaseRepository<Keyword> {
+  constructor(@InjectModel(Keyword.name) keywordModel: Model<Keyword>) {
+    super(keywordModel)
   }
 }
--- src/modules/keyword/services/keyword.service.ts
+++ src/modules/keyword/services/keyword.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Inject, Injectable, NotFoundException, forwardRef } from '@nestjs/common'
+import { DocumentIdType, ResultMessage } from '@src/shared/contracts/types'
 import { PostService } from '@src/modules/post/services'
 import { KeywordRepository } from '../repositories'
-import { DocumentIdType, ResultMessage } from '@src/shared/contracts/types'
 import { CreateKeywordDto } from '../dtos'
 import { MESSAGES } from '../constants'
 import { Keyword } from '../schemas'
@@ -27,7 +27,9 @@ export class KeywordService {
     await this.isKeywordAvailable(keywordId)
     await this.isKeywordAssociatedToPosts(keywordId)
 
-    return await this.keywordRepo.deleteOne(keywordId)
+    await this.keywordRepo.deleteOne(keywordId)
+
+    return { message: MESSAGES.DELETED_SUCCESSFULLY }
   }
 
   public async isKeywordAssociatedToPosts(keywordId: DocumentIdType): Promise<void> {
@@ -36,23 +38,21 @@ export class KeywordService {
     if (isKeywordAssociated) throw new BadRequestException(MESSAGES.KEYWORD_ASSOCIATED_TO_POST)
   }
 
-  public async isKeywordAvailable(keywordId: DocumentIdType): Promise<Keyword> {
-    return await this.keywordRepo.findOneById(keywordId)
+  public async isKeywordAvailable(keywordId: DocumentIdType): Promise<boolean> {
+    return await this.keywordRepo.isFound({ _id: keywordId })
   }
 
-  public async areKeywordsAvailable(tags: DocumentIdType[]): Promise<void> {
-    try {
-      await Promise.all(tags.map((tag) => this.isKeywordAvailable(tag)))
-    } catch (error) {
-      throw new NotFoundException(MESSAGES.NOT_AVAILABLE)
-    }
+  public async areKeywordsAvailable(keywords: DocumentIdType[]): Promise<void> {
+    const areAvailable = await Promise.all(keywords.map((keyword) => this.isKeywordAvailable(keyword)))
+
+    if (areAvailable.includes(false)) throw new NotFoundException(MESSAGES.NOT_AVAILABLE)
   }
 
   public async getAllKeywords(): Promise<Keyword[]> {
-    return await this.keywordRepo.findMany()
+    return await this.keywordRepo.find({})
   }
 
   public async getAllKeywordsCount(): Promise<ResultMessage> {
-    return await this.keywordRepo.countDocuments()
+    return await this.keywordRepo.countDocuments({})
   }
 }
