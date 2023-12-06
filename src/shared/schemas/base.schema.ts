6f01b04ac856955f38c65dc0599ed012d6789fd2
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-06 14:08:14 +0200

    Did some refactoring at the BaseSchema

--- src/shared/schemas/base.schema.ts
+++ src/shared/schemas/base.schema.ts
@@ -1,9 +1,5 @@
 import { DocumentIdType } from '@src/shared/contracts/types'
-import { Prop, Schema } from '@nestjs/mongoose'
-import { Types } from 'mongoose'
 
-@Schema()
 export class BaseSchema {
-  @Prop({ type: Types.ObjectId })
   _id: DocumentIdType
 }
